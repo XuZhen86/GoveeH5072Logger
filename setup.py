62b841d7df6c41d9a00d54938cf421d67eb6b54a
Zhen Xu <curt_xu@me.com>
2023-02-14 23:23:58 -0500

    Update Line Protocol Cache to a7965f30589af8c318ff7654d307ca9ec7d9f40f.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
         'absl-py>=1.3.0',
         'bleak>=0.19.5',
         'influxdb-client>=1.35.0',
-        'line_protocol_cache@git+https://github.com/XuZhen86/LineProtocolCache@26ade43329fd143fd2311926620e5a01082be1e1',
+        'line_protocol_cache@git+https://github.com/XuZhen86/LineProtocolCache@a7965f30589af8c318ff7654d307ca9ec7d9f40f',
     ],
     entry_points={
         'console_scripts': [
